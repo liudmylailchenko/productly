727f5405a5a5bcdfe8fbe87a32dcd65923606824
Liudmyla Ilchenko <lydailchenco@gmail.com>
2020-04-02 11:17:28 +0300

    Added new style to responsive header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,21 @@
 import React from 'react';
 import styled from 'styled-components';
 import logo from '../images/Logo.svg';
+import menu from '../images/align-justify-solid.svg';
+import times from '../images/times-solid.svg';
 import Button from './Button';
+import Container from './Container';
 
 const Wrapper = styled.header`
   padding: 25px 0;
+
+  @media screen and (max-width: 800px) {
+    position: relative;
+    border-bottom: 1px solid #bcd0e5;
+  }
 `;
 
-const Container = styled.div`
-  max-width: 1130px;
-  margin: 0 auto;
+const ContainerStyled = styled(Container)`
   display: flex;
   align-items: center;
 `;
@@ -18,9 +24,53 @@ const LogoWraper = styled.div`
   flex-grow: 1;
 `;
 
+const Menu = styled.div`
+  display: flex;
+
+  @media screen and (max-width: 800px) {
+    display: none;
+    position: absolute;
+    left: 0;
+    right: 0;
+    top: 100%;
+    padding: 10px;
+    flex-direction: column;
+    align-items: center;
+    border-bottom: 1px solid #bcd0e5;
+
+    &.active {
+      display: flex;
+    }
+  }
+`;
+
+const MenuButton = styled.button`
+  display: none;
+
+  @media screen and (max-width: 800px) {
+    display: block;
+    background: none;
+    border: none;
+    outline: none;
+
+    :hover {
+      cursor: pointer;
+    }
+
+    img {
+      width: 25px;
+      height: 25px;
+    }
+  }
+`;
+
 const ButtonsWrapper = styled.div`
   button {
     margin-left: 14px;
+  }
+
+  @media screen and (max-width: 800px) {
+    padding: 10px;
   }
 `;
 
@@ -37,32 +87,55 @@ const Navigation = styled.button`
     cursor: pointer;
     text-decoration: underline;
   }
+
+  @media screen and (max-width: 800px) {
+    padding: 10px;
+  }
 `;
 
-function Header() {
-  return (
-    <Wrapper>
-      <Container>
-        <LogoWraper>
-          <img src={logo} alt="" />
-        </LogoWraper>
+class Header extends React.Component {
+  state = {
+    activeMenu: false,
+  };
 
-        <Navigation>Product</Navigation>
-        <Navigation>Customers</Navigation>
-        <Navigation>Pricng</Navigation>
-        <Navigation>Resources</Navigation>
+  handleClick = () => {
+    const { activeMenu } = this.state;
+    const changeActiveMenu = activeMenu === false ? true : false;
+    this.setState({
+      activeMenu: changeActiveMenu,
+    });
+  };
 
-        <ButtonsWrapper>
-          <Button variant="secondary" size="md">
-            Sign in
-          </Button>
-          <Button variant="primary" size="md">
-            Sign up
-          </Button>
-        </ButtonsWrapper>
-      </Container>
-    </Wrapper>
-  );
+  render() {
+    const { activeMenu } = this.state;
+    return (
+      <Wrapper>
+        <ContainerStyled>
+          <LogoWraper>
+            <img src={logo} alt="" />
+          </LogoWraper>
+          <Menu className={activeMenu === true ? 'active' : ''}>
+            <Navigation>Product</Navigation>
+            <Navigation>Customers</Navigation>
+            <Navigation>Pricng</Navigation>
+            <Navigation>Resources</Navigation>
+
+            <ButtonsWrapper>
+              <Button variant="secondary" size="md">
+                Sign in
+              </Button>
+              <Button variant="primary" size="md">
+                Sign up
+              </Button>
+            </ButtonsWrapper>
+          </Menu>
+          <MenuButton onClick={this.handleClick}>
+            <img src={activeMenu === false ? menu : times} alt="" />
+          </MenuButton>
+        </ContainerStyled>
+      </Wrapper>
+    );
+  }
 }
 
 export default Header;
