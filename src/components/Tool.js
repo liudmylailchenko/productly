4edc37906eb841d7ce3c1aaf3ec450b77c9fea8d
liudmylailchenko <59619067+liudmylailchenko@users.noreply.github.com>
2020-04-07 21:18:45 +0300

    Added new style to responsive section design tools (#3)
    * Added new style to responsive section design tools
    
    * Reviewe fixes

--- src/components/Tool.js
+++ src/components/Tool.js
@@ -3,8 +3,21 @@ import styled from 'styled-components/macro';
 import { H4, TextSm } from './Typography';
 
 const Wrapper = styled.div`
-  padding: 0 20px;
-  flex: 1 1 100%;
+  padding: 20px;
+  text-align: center;
+
+  h4 {
+    margin: 10px 0;
+  }
+
+  @media screen and (min-width: 660px) {
+    padding: 0 20px;
+    flex: 1 1 100%;
+
+    h4 {
+      margin: 22px 0;
+    }
+  }
 `;
 
 const Content = styled.div``;
