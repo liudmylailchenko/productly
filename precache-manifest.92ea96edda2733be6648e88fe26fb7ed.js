ebb76d50a6dab26e1a47fb079d80a6c3862add55
Liudmyla Ilchenko <lydailchenco@gmail.com>
2020-04-15 02:36:53 +0300

    Updates

---
+++ precache-manifest.92ea96edda2733be6648e88fe26fb7ed.js
@@ -0,0 +1,122 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ded9ac4c90411acc10296ad622d33cd1",
+    "url": "/productly/index.html"
+  },
+  {
+    "revision": "da18f2f7a07bc5ca8a18",
+    "url": "/productly/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "0ac9a303916b7ae676f5",
+    "url": "/productly/static/css/main.439af788.chunk.css"
+  },
+  {
+    "revision": "da18f2f7a07bc5ca8a18",
+    "url": "/productly/static/js/2.ee0c29a6.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/productly/static/js/2.ee0c29a6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0ac9a303916b7ae676f5",
+    "url": "/productly/static/js/main.e25a7732.chunk.js"
+  },
+  {
+    "revision": "a2d99edbb290bbaafb0f",
+    "url": "/productly/static/js/runtime-main.ae7f0b5c.js"
+  },
+  {
+    "revision": "0caa38289848d18fe390c12213ced6eb",
+    "url": "/productly/static/media/Logo.0caa3828.svg"
+  },
+  {
+    "revision": "179bf66acba14632d600aaa1d649e8ee",
+    "url": "/productly/static/media/align-justify-solid.179bf66a.svg"
+  },
+  {
+    "revision": "e9e8dee2bb0f3e975e2c7c587099958e",
+    "url": "/productly/static/media/app.e9e8dee2.png"
+  },
+  {
+    "revision": "16ef2e8933d8c5f913abd62cf6b2bd48",
+    "url": "/productly/static/media/arrow.16ef2e89.svg"
+  },
+  {
+    "revision": "d0bdf9ff8418c5eb9bda309d0dc8bc30",
+    "url": "/productly/static/media/background1.d0bdf9ff.svg"
+  },
+  {
+    "revision": "5bdc69f2ced1972381115913cb917567",
+    "url": "/productly/static/media/background2.5bdc69f2.svg"
+  },
+  {
+    "revision": "ccdfebfbafd67c42e97289c0168ee59c",
+    "url": "/productly/static/media/cardimage1.ccdfebfb.svg"
+  },
+  {
+    "revision": "891de31ef224ec5598b3366aa1f16fc4",
+    "url": "/productly/static/media/cardimage2.891de31e.svg"
+  },
+  {
+    "revision": "3d7c2a99ab43d86a1632356558ed3185",
+    "url": "/productly/static/media/cardimage3.3d7c2a99.svg"
+  },
+  {
+    "revision": "51913b3b30787b987861b2dbbef7baf3",
+    "url": "/productly/static/media/check.51913b3b.svg"
+  },
+  {
+    "revision": "4e0ad126cf4e274b4e472a9f923433db",
+    "url": "/productly/static/media/clock.4e0ad126.svg"
+  },
+  {
+    "revision": "297f0d125bf49f09c9975dbc95c1b8fa",
+    "url": "/productly/static/media/design.297f0d12.png"
+  },
+  {
+    "revision": "31b793c2e19a9f4b890f253f1bdf4025",
+    "url": "/productly/static/media/designprofessionals.31b793c2.svg"
+  },
+  {
+    "revision": "69f52394365ec82520a01e8e3b7feee8",
+    "url": "/productly/static/media/designthinking.69f52394.svg"
+  },
+  {
+    "revision": "e162b1e9798a377c632ccc271abdeac2",
+    "url": "/productly/static/media/heart.e162b1e9.svg"
+  },
+  {
+    "revision": "54aa72c8bd267d7318b6908ff740165b",
+    "url": "/productly/static/media/icon.54aa72c8.svg"
+  },
+  {
+    "revision": "dad6d1e2dd8c3a70b3a13efdf7265f0a",
+    "url": "/productly/static/media/marketers.dad6d1e2.svg"
+  },
+  {
+    "revision": "ac620894443e943ceee25669dabd452d",
+    "url": "/productly/static/media/marketing.ac620894.png"
+  },
+  {
+    "revision": "2e7e6a114320c9dc07330b9a4ebf2410",
+    "url": "/productly/static/media/pitch.2e7e6a11.png"
+  },
+  {
+    "revision": "7ab22939c3137dc4ed9ff6625c5a02ad",
+    "url": "/productly/static/media/productmanagers.7ab22939.svg"
+  },
+  {
+    "revision": "2c5552a10c3ab0a115c9484f908f982d",
+    "url": "/productly/static/media/times-solid.2c5552a1.svg"
+  },
+  {
+    "revision": "85b088f28bd850af3251ee8e2244084f",
+    "url": "/productly/static/media/triangle.85b088f2.svg"
+  },
+  {
+    "revision": "90c755c4e087e11e764c17669a474bd3",
+    "url": "/productly/static/media/users.90c755c4.png"
+  }
+]);
